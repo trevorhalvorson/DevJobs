e418e1c57832eae9e36ddf1a43763c99270a4bbe
Trevor Halvorson <trevor.halvorson@gmail.com>
2015-07-21 07:15:15 -0700

    Update  simple List to swipeable RecyclerView Add menu item to toggle search View

--- app/src/main/java/com/trevorhalvorson/devjobs/MainActivity.java
+++ app/src/main/java/com/trevorhalvorson/devjobs/MainActivity.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.trevorhalvorson.devjobs.model.Job;
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final String ENDPOINT = "https://jobs.github.com";
     private RecyclerView recyclerView;
+    private LinearLayout searchView;
     private ArrayList<Job> jobList;
     private JobAdapter adapter;
 
@@ -36,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText searchLoc = (EditText) findViewById(R.id.editLoc);
         recyclerView = (RecyclerView) findViewById(R.id.jobListView);
         recyclerView.setVisibility(View.GONE);
+        searchView = (LinearLayout) findViewById(R.id.linearLayout);
         Button searchButton = (Button) findViewById(R.id.searchButton);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,11 +81,50 @@ public class MainActivity extends AppCompatActivity {
             recyclerView.setVisibility(View.VISIBLE);
             recyclerView.setAdapter(adapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            searchView.setVisibility(View.GONE);
         } else {
             Toast.makeText(MainActivity.this, "Sorry, no jobs found", Toast.LENGTH_SHORT).show();
         }
 
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                jobList.remove(viewHolder.getAdapterPosition());
+                adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+
+                switch (direction) {
+                    case ItemTouchHelper.LEFT:
+                        //TODO: Save job to device using SQLite
+                        break;
+                    case ItemTouchHelper.RIGHT:
+                        //TODO: Remove job from current and future searches using jobId
+                }
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                searchView.setVisibility(View.VISIBLE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
