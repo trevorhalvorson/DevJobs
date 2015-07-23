8ba32c08fdf73234f9d34506e93dacd6d6e593f4
Trevor Halvorson <trevor.halvorson@gmail.com>
2015-07-22 22:18:28 -0700

    Switch from Activities to Fragments in Main and JobDetail classes Add SwipeToRefresh on RecyclerView Add screen for Job details

--- app/src/main/java/com/trevorhalvorson/devjobs/JobAdapter.java
+++ app/src/main/java/com/trevorhalvorson/devjobs/JobAdapter.java
@@ -2,14 +2,15 @@ package com.trevorhalvorson.devjobs;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import com.trevorhalvorson.devjobs.model.Job;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,11 +18,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
-/**
- * Created by Trevor on 7/16/2015.
- */
 public class JobAdapter extends RecyclerView.Adapter<JobAdapter.JobViewHolder> {
-
     private static final String TAG = JobAdapter.class.getSimpleName();
     private final LayoutInflater inflater;
     private Context context;
@@ -47,14 +44,23 @@ public class JobAdapter extends RecyclerView.Adapter<JobAdapter.JobViewHolder> {
         holder.jobTitle.setText(currentJob.getTitle());
         holder.jobCompany.setText(currentJob.getCompany());
         holder.jobLocation.setText(currentJob.getLocation());
-        holder.jobDate.setText(getDateSpan(currentJob.getCreated_at()));
+        holder.jobDate.setText(getDateSpan(currentJob.getCreated_at()).toString());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse(currentJob.getUrl()));
+
+                Intent intent = new Intent(context, JobDetailActivity.class);
+                intent.putExtra(JobDetailActivity.EXTRA_TITLE, currentJob.getTitle());
+                intent.putExtra(JobDetailActivity.EXTRA_DESCRIPTION, currentJob.getDescription());
+                intent.putExtra(JobDetailActivity.EXTRA_COMPANY, currentJob.getCompany());
+                intent.putExtra(JobDetailActivity.EXTRA_LOCATION, currentJob.getLocation());
+                intent.putExtra(JobDetailActivity.EXTRA_DATE, getDateSpan(currentJob.getCreated_at()));
+                intent.putExtra(JobDetailActivity.EXTRA_URL, currentJob.getUrl());
                 context.startActivity(intent);
             }
+
+
         });
     }
 
--- app/src/main/java/com/trevorhalvorson/devjobs/MainActivity.java
+++ app/src/main/java/com/trevorhalvorson/devjobs/MainActivity.java
@@ -2,112 +2,26 @@ package com.trevorhalvorson.devjobs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.trevorhalvorson.devjobs.model.Job;
-
-import java.util.ArrayList;
-
-import retrofit.Callback;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final String ENDPOINT = "https://jobs.github.com";
-    private RecyclerView recyclerView;
-    private LinearLayout searchView;
-    private ArrayList<Job> jobList;
-    private JobAdapter adapter;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final EditText searchDesc = (EditText) findViewById(R.id.editDesc);
-        final EditText searchLoc = (EditText) findViewById(R.id.editLoc);
-        recyclerView = (RecyclerView) findViewById(R.id.jobListView);
-        recyclerView.setVisibility(View.GONE);
-        searchView = (LinearLayout) findViewById(R.id.linearLayout);
-        Button searchButton = (Button) findViewById(R.id.searchButton);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String searchJobDesc = searchDesc.getText().toString();
-                String searchLocText = searchLoc.getText().toString();
-                searchJobTask(searchJobDesc, searchLocText);
-            }
-        });
-
-    }
-
-    public void searchJobTask(String search, String location) {
-        RestAdapter adapter = new RestAdapter.Builder()
-                .setEndpoint(ENDPOINT)
-                .build();
-
-        GHJobsAPI api = adapter.create(GHJobsAPI.class);
-        api.getGHJobs(search, location, new Callback<ArrayList<Job>>() {
-            @Override
-            public void success(ArrayList<Job> jobs, Response response) {
-                jobList = jobs;
-                updateDisplay();
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d(TAG, "failure: " + error.toString());
-            }
-        });
-    }
-
-    private void updateDisplay() {
-        if (!jobList.isEmpty()) {
-            adapter = new JobAdapter(getApplicationContext(), jobList);
-            recyclerView.setVisibility(View.VISIBLE);
-            recyclerView.setAdapter(adapter);
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-            searchView.setVisibility(View.GONE);
-        } else {
-            Toast.makeText(MainActivity.this, "Sorry, no jobs found", Toast.LENGTH_SHORT).show();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new JobListFragment())
+            .commit();
         }
 
-        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-            @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                return false;
-            }
 
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-                jobList.remove(viewHolder.getAdapterPosition());
-                adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
-
-                switch (direction) {
-                    case ItemTouchHelper.LEFT:
-                        //TODO: Save job to device using SQLite
-                        break;
-                    case ItemTouchHelper.RIGHT:
-                        //TODO: Remove job from current and future searches using jobId
-                }
-            }
-        };
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
-        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 
     @Override
@@ -120,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_search:
-                searchView.setVisibility(View.VISIBLE);
+            case R.id.action_refresh:
+                // TODO: refresh recyclerView
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
